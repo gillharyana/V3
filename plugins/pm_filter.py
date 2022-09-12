56ebc825e87132780056fe78bd8766f0751f5f25
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-12 10:03:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -91,22 +91,16 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("🔥 How to Download", "https://t.me/how_to_watchlink"),
-             InlineKeyboardButton("Help ✨", "https://t.me/HindiFilmsBot"),],
             [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
-            [InlineKeyboardButton("🔥 How to Download", "https://t.me/how_to_watchlink"),
-             InlineKeyboardButton("Help ✨", "https://t.me/HindiFilmsBot"),],
             [InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
              InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
-            [InlineKeyboardButton("🔥 How to Download", "https://t.me/how_to_watchlink"),
-             InlineKeyboardButton("Help ✨", "https://t.me/HindiFilmsBot"),],
             [
                 InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
@@ -672,17 +666,13 @@ async def auto_filter(client, msg, spoll=False):
         BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton("🔥 How to Download", "https://t.me/how_to_watchlink"),
-             InlineKeyboardButton("Help ✨", "https://t.me/HindiFilmsBot"),],
             [InlineKeyboardButton(text=f"🗓 1/{math.ceil(int(total_results) / 10)}", callback_data="pages"),
              InlineKeyboardButton(text="NEXT ⏩", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
-            [InlineKeyboardButton("🔥 How to Download", "https://t.me/how_to_watchlink"),
-             InlineKeyboardButton("Help ✨", "https://t.me/HindiFilmsBot"),],
             [InlineKeyboardButton(text="🗓 1/1", callback_data="pages")]
-             )   
+             )       
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
