d158eb60e5cea8014b74c083750d56eaeeacc66f
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-09-26 08:34:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,9 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
+    START_TXT = """**Hello {},
+My Name is <a href=https://t.me/{}>{}</a>, I can provide movie 🎬, Just Add me to your group and enjoy 😍**"""
+    HELP_TXT = """Hello {}
+➛ Add me in your group and just type movie name and enjoy or 
+➛ Type Movie name in this bot and follow @how_to_watchlink to download movie 🍿"""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/VasuxD>ㅤ⏤͟͞ ⚝ ‌❛ ᴠᴀꜱᴜ™ ❜⏤͟͟͞͞ ✧</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
