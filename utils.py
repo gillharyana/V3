27b266d9735df3e4850f276af10b9426e17fdb45
GreyMatter-Test <117898858+GreyMatter-Test@users.noreply.github.com>
2022-12-03 20:10:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -374,3 +374,29 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+
+    url = f'https://api.shareus.in/shortLink'
+    params = {'token': URL_SHORTNER_WEBSITE_API,
+              'link': link,
+              'format': 'json'
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json(content_type='text/html')
+                if data["status"] == "success":
+                    return data['shortlink']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
