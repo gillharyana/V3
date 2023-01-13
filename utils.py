c6936a0e3b482ff916a70619da878aa7458becc9
gillharyana <100374349+gillharyana@users.noreply.github.com>
2023-01-13 20:44:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -394,8 +394,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://api.mb.masterbaba.in.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://api.mb.masterbaba.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
